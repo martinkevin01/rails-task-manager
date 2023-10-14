fb1b0589acb0ce31c4450eeaa883c8e5e5a86ea8
Martin Kevin Gapie <martinkevingapie@gmail.com>
2023-10-14 14:39:16 -0400

    first version crud

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id/edit', to: 'tasks#edit'
+
+  get 'tasks/:id', to: 'tasks#show', as: :show
+  post 'tasks', to: 'tasks#create'
+
+  patch 'tasks/:id', to: 'tasks#update'
 end
